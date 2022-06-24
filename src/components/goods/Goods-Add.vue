e1e266b24d750f800f563644fd919ab70a53b923
liulichao <liulichao@renrencai.com>
2022-06-24 12:00:15 +0800

    表单设置

--- src/components/goods/Goods-Add.vue
+++ src/components/goods/Goods-Add.vue
@@ -9,7 +9,7 @@
       <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
       </el-alert>
       <!-- 步骤条 -->
-      <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -17,15 +17,29 @@
         <el-step title="商品内容"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
+      <!-- 左侧tab栏 -->
+      <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="100px" label-position="top">
+        <el-tabs tab-position="left" v-model="activeIndex" style="height: 200px;">
+          <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
       // 激活的步骤
-      activeIndex: 0
+      activeIndex: 0,
+      // 添加商品
+      addForm: {},
+      // 添加商品校验
+      addFormRules: {}
     }
   }
 }
