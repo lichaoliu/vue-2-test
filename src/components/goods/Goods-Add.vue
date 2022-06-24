00f2edaeee41c87c62d6d2f649dbafb3e5c64f67
lichaoliu <1147149597@qq.com>
2022-06-24 23:31:39 +0800

    添加商品分类

--- src/components/goods/Goods-Add.vue
+++ src/components/goods/Goods-Add.vue
@@ -19,8 +19,25 @@
       </el-steps>
       <!-- 左侧tab栏 -->
       <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="100px" label-position="top">
-        <el-tabs tab-position="left" v-model="activeIndex" style="height: 200px;">
-          <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tabs tab-position="left" v-model="activeIndex">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price" type="number">
+              <el-input v-model="addForm.goods_price"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight" type="number">
+              <el-input v-model="addForm.goods_weight"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number" type="number">
+              <el-input v-model="addForm.goods_number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat" type="number">
+              <el-cascader expandTrigger="hover" :props="cateProps" :options="cateList" v-model="addForm.goods_cat"
+                @change="handleChange"></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
           <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
           <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
           <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
@@ -32,16 +49,64 @@
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
       // 激活的步骤
       activeIndex: 0,
       // 添加商品
-      addForm: {},
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        // 商品分类数组
+        goods_cat: []
+      },
       // 添加商品校验
-      addFormRules: {}
+      addFormRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价格', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' }
+        ],
+        goods_cat: [
+          { required: true, message: '请选择商品分类', trigger: 'blur' }
+        ]
+      },
+      // 商品分类
+      cateList: [],
+      cateProps: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      }
+    }
+  },
+  created () {
+    this.getCateList()
+  },
+  // 获取商品分类
+  methods: {
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败')
+      }
+      this.cateList = res.data
+    },
+    // 级联选择器选中项变化 会触发这个函数
+    handleChange () {
+      console.log(this.addForm.goods_cat);
     }
   }
+
 }
 </script>
 <style lang="less" scoped>
