5e72aa2db029473a69e597e59a961871323500fb
lichaoliu <1147149597@qq.com>
2022-06-26 16:35:07 +0800

    添加商品

--- src/components/goods/Goods-Add.vue
+++ src/components/goods/Goods-Add.vue
@@ -19,7 +19,7 @@
       </el-steps>
       <!-- 左侧tab栏 -->
       <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="100px" label-position="top">
-        <el-tabs tab-position="left" v-model="activeIndex">
+        <el-tabs tab-position="left" v-model="activeIndex" :before-leave="beforeTabLeave" @tab-click="tabClicked">
           <el-tab-pane label="基本信息" name="0">
             <el-form-item label="商品名称" prop="goods_name">
               <el-input v-model="addForm.goods_name"></el-input>
@@ -38,7 +38,13 @@
                 @change="handleChange"></el-cascader>
             </el-form-item>
           </el-tab-pane>
-          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+            <el-form-item :label="item.attr_name" v-for="item in this.manyTableData" :key="item.attr_id">
+              <el-checkbox-group v-model="item.attr_vals">
+                <el-checkbox :label="cb" v-for="(cb, i) in item.attr_vals" :key="i"></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
           <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
           <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
           <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
@@ -86,7 +92,9 @@ export default {
         label: 'cat_name',
         value: 'cat_id',
         children: 'children'
-      }
+      },
+      // 动态参数列表数据
+      manyTableData: []
     }
   },
   created () {
@@ -103,7 +111,40 @@ export default {
     },
     // 级联选择器选中项变化 会触发这个函数
     handleChange () {
-      console.log(this.addForm.goods_cat);
+      console.log(this.addForm.goods_cat)
+      if (this.addForm.goods_cat.length == 2) {
+        this.addForm.goods_cat = []
+      }
+    },
+    // tab切换触发的函数
+    beforeTabLeave (activeName, oldActiveName) {
+      if (oldActiveName === '0' && activeName === '1' && this.addForm.goods_cat.length !== 3) {
+        this.$message.error('请选择商品分类')
+        return false
+      }
+    },
+    async tabClicked () {
+      if (this.activeIndex === '1') {
+        const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: 'many' } })
+        if (res.meta.status !== 200) {
+          console.log(res);
+          return this.$message.error('获取分类参数失败')
+        }
+        res.data.forEach(item => {
+          item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+        })
+        this.manyTableData = res.data
+        console.log(this.manyTableData)
+      }
+    }
+  },
+  // 三级商品分类id
+  computed: {
+    cateId () {
+      if (this.addForm.goods_cat.length === 3) {
+        return this.addForm.goods_cat[2]
+      }
+      return null
     }
   }
 
