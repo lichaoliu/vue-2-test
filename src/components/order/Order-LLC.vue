030ee106bf4e98b1ec4f70612982103c03d31523
liulichao <liulichao@renrencai.com>
2022-06-28 18:41:26 +0800

    物流进度

--- src/components/order/Order-LLC.vue
+++ src/components/order/Order-LLC.vue
@@ -32,34 +32,44 @@
         </el-table-column>
         <el-table-column label="操作">
           <el-button type="primary" size="mini" icon="el-icon-edit" @click="showBox"></el-button>
-          <el-button type="success" size="mini" icon="el-icon-location"></el-button>
+          <el-button type="success" size="mini" icon="el-icon-location" @click="showProgressBox"></el-button>
         </el-table-column>
       </el-table>
-
-      <!-- 修改地址对话框 -->
-      <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @closed="addressDialogClosed">
-        <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
-          <el-form-item label="省市区/县" prop="address1">
-            <el-cascader v-model="addressForm.address1" :options="citydata">
-            </el-cascader>
-          </el-form-item>
-          <el-form-item label="详细地址" prop="address2">
-            <el-input v-model="addressForm.address2"></el-input>
-          </el-form-item>
-        </el-form>
-        <span slot="footer">
-          <el-button @click="addressVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addressVisible = false">确 定</el-button>
-        </span>
-      </el-dialog>
     </el-card>
+
+    <!-- 修改地址对话框 -->
+    <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @closed="addressDialogClosed">
+      <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader v-model="addressForm.address1" :options="citydata">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer">
+        <el-button @click="addressVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addressVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 展示物流信息对话框 -->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <el-timeline>
+        <el-timeline-item v-for="(activity, index) in progressInfo" :key="index" :timestamp="activity.time">
+          {{ activity.context }}
+        </el-timeline-item>
+      </el-timeline>
+    </el-dialog>
+
   </div>
 </template>
 <script>
 import citydata from './citydata.js'
 
 export default {
-  data () {
+  data() {
     return {
       quryInfo: {
         query: '',
@@ -83,14 +93,86 @@ export default {
           { required: true, message: '请填写详细地址', trigger: 'blur' }
         ]
       },
-      citydata
+      citydata,
+      // 物流进度对话框展示
+      progressVisible: false,
+      // 物流进度
+      progressInfo: [],
+      progressObj: {
+        'data': [
+          {
+            'time': '2018-05-10 09:39:00',
+            'ftime': '2018-05-10 09:39:00',
+            'context': '已签收,感谢使用顺丰,期待再次为您服务',
+            'location': ''
+          },
+          {
+            'time': '2018-05-10 08:23:00',
+            'ftime': '2018-05-10 08:23:00',
+            'context': '[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件',
+            'location': ''
+          },
+          {
+            'time': '2018-05-10 07:32:00',
+            'ftime': '2018-05-10 07:32:00',
+            'context': '快件到达 [北京海淀育新小区营业点]',
+            'location': ''
+          },
+          {
+            'time': '2018-05-10 02:03:00',
+            'ftime': '2018-05-10 02:03:00',
+            'context': '快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]',
+            'location': ''
+          },
+          {
+            'time': '2018-05-09 23:05:00',
+            'ftime': '2018-05-09 23:05:00',
+            'context': '快件到达 [北京顺义集散中心]',
+            'location': ''
+          },
+          {
+            'time': '2018-05-09 21:21:00',
+            'ftime': '2018-05-09 21:21:00',
+            'context': '快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]',
+            'location': ''
+          },
+          {
+            'time': '2018-05-09 13:07:00',
+            'ftime': '2018-05-09 13:07:00',
+            'context': '顺丰速运 已收取快件',
+            'location': ''
+          },
+          {
+            'time': '2018-05-09 12:25:03',
+            'ftime': '2018-05-09 12:25:03',
+            'context': '卖家发货',
+            'location': ''
+          },
+          {
+            'time': '2018-05-09 12:22:24',
+            'ftime': '2018-05-09 12:22:24',
+            'context': '您的订单将由HLA（北京海淀区清河中街店）门店安排发货。',
+            'location': ''
+          },
+          {
+            'time': '2018-05-08 21:36:04',
+            'ftime': '2018-05-08 21:36:04',
+            'context': '商品已经下单',
+            'location': ''
+          }
+        ],
+        'meta': {
+          'status': 200,
+          'message': '获取物流信息成功！'
+        }
+      }
     }
   },
-  created () {
+  created() {
     this.getOrderList()
   },
   methods: {
-    async getOrderList () {
+    async getOrderList() {
       const { data: res } = await this.$http.get('orders', { params: this.quryInfo })
       if (res.meta.status !== 200) {
         return this.$message.error('获取订单列表失败')
@@ -100,11 +182,21 @@ export default {
       console.log(this.orderList)
       console.log(this.total)
     },
-    showBox () {
+    showBox() {
       this.addressVisible = true
     },
-    addressDialogClosed () {
+    addressDialogClosed() {
+      console.log(this.$refs.addressFormRef);
       this.$refs.addressFormRef.resetFields()
+    },
+    // 查询物流信息
+    showProgressBox() {
+      const res = this.progressObj
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取物流进度失败')
+      }
+      this.progressInfo = res.data
+      this.progressVisible = true
     }
   }
 }
