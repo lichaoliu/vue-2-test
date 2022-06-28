b5b3492bfeb4c40d583faa76b94f5feac99b8fbf
lichaoliu <1147149597@qq.com>
2022-06-28 23:34:36 +0800

    优化项目

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
+// 这是项目发布阶段需要用到的 babel 插件
+const prodPlugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  // 发布项目用到的插件数组
+  plugins: [
+    ...prodPlugins
   ]
 }
--- src/main.js
+++ src/main.js
@@ -16,9 +16,19 @@ import 'quill/dist/quill.bubble.css' // for bubble theme
 import axios from 'axios'
 Vue.prototype.$http = axios
 
+// 进度条使用
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
+  // NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+axios.interceptors.request.use(config => {
+  // NProgress.done()
   return config
 })
 
