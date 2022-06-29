4a0ea734dce4cf06b9d466f478920ee89d7d9a9e
liulichao <liulichao@renrencai.com>
2022-06-29 17:10:08 +0800

    使用externals打包

---
+++ src/main-prod.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import VueQuillEditor from 'vue-quill-editor'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+
+// 进度条使用
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+axios.interceptors.request.use(config => {
+  // NProgress.done()
+  return config
+})
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueQuillEditor)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        lodash: '_',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        'vue-quill-editor': 'vueQuillEditor'
+      })
+
+    })
+  }
+}
